50d1fc2b362f6a1ca15a71ffeba30b00e75a5007
bisawanucleAR <152012913+bisawanucleAR@users.noreply.github.com>
2025-05-31 10:25:40 -0300

    31/05/2025

---
+++ aula13/script.js
@@ -0,0 +1,41 @@
+
+
+//let listatipos = ['Agua','Planta','Voador','Fogo','Dragao'];
+
+//listatipos.push('Fada');
+//listatipos.unshift('Lutador')
+
+//document.(listatipos);
+
+//let temp = prompt("Digite um numero");
+
+//if (temp = 35){
+//    document.writeln("Está muito quente");
+//} else if (temp> 25){
+//    document.writeln("Está agradavel");
+//} else {
+//    document.writeln("Está frio");
+//}
+
+// for(let contador = 0; contador < 90; contador++){
+//     document.writeln(contador)
+// }
+
+// let tipos = ['Agua','Planta','Fogo'];
+
+// for(let tipo in tipos){
+//     document.writeln(tipo+ ":"+tipos[tipo])
+// }
+
+// for(let tipo of tipos){
+//     document.writeln(tipo)
+// }//percorre os valores de cada
+
+
+let senha = "";
+
+while (senha !== "abc123"){
+    senha = prompt("Digite a senha:");
+}
+
+document.writeln("TA LIBERADO CHEFE")
